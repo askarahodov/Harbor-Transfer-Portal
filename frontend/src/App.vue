3ea343d98a8036e421ac88af1c4726663eaf6708
askarahodov <devops.bel@yandex.ru>
2026-09-11 09:42:31 +0300

    refactor(frontend): separate bare and application layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,10 @@ import PageShell from '@/components/PageShell.vue'
 </script>
 
 <template>
-  <PageShell>
-    <RouterView />
-  </PageShell>
+  <RouterView v-slot="{ Component, route }">
+    <component :is="Component" v-if="route.meta.layout === 'bare'" />
+    <PageShell v-else>
+      <component :is="Component" />
+    </PageShell>
+  </RouterView>
 </template>
