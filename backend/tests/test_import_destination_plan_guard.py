b5adbeb50e5d07c8b70d9a9b94fecddc20565ffc
askarahodov <devops.bel@yandex.ru>
2026-09-15 10:39:22 +0300

    test: remove redundant import guard

--- backend/tests/test_import_destination_plan_guard.py
+++
@@ -1,5 +0,0 @@
-from app.services.import_destination_plan import ImportDestinationPlanOrchestrator
-
-
-def test_destination_plan_orchestrator_imports() -> None:
-    assert ImportDestinationPlanOrchestrator.__name__ == "ImportDestinationPlanOrchestrator"
