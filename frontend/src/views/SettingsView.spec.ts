2a488ad19af76ae393af5177916fdb483dfae6d6
askarahodov <devops.bel@yandex.ru>
2026-09-22 16:04:07 +0300

    test(ui): target default Harbor form explicitly (#361)

--- frontend/src/views/SettingsView.spec.ts
+++ frontend/src/views/SettingsView.spec.ts
@@ -150,7 +150,7 @@ describe('Harbor settings view', () => {
     )
 
     await wrapper.get('#harbor-url').setValue('https://new.harbor.local')
-    await wrapper.find('form:not(.transfer-form)').trigger('submit')
+    await wrapper.get('.harbor-default-form').trigger('submit')
     await flushPromises()
 
     expect(patch).toHaveBeenCalledWith('/settings/harbor', {
